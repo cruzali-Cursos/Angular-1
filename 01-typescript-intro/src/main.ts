c40c57ed848161666057615c4eb96424b8ef7ac0
ali.cruz@me.com <ali.cruz@me.com>
2024-11-24 08:23:49 -0600

    intro, conceptos generales, bases de typescript 1/x

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,13 @@
+import './style.css'
+import './topics/01-basic-types';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  Hola Mundo
+
+
+  </div>
+`
+
+
+console.log('Hola Mundo')
---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,9 @@
+const name = 'strider';
+let hpPoints: number | 'FULL' | boolean = 95;
+hpPoints = 'FULL';
+
+const isAlive: boolean = true;
+
+console.log({name, hpPoints, isAlive});
+
+export {};
